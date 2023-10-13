d014fb5204489381256fa4b117ba4fa552689390
kazu-gor <ik126@icloud.com>
2023-10-13 16:57:15 +0900

    modified

---
+++ _posts/001_最初の記事.md
@@ -0,0 +1,15 @@
+---
+title: 'kazu-gor'
+excerpt: 'はい。'
+coverImage: '/assets/blog/001/white.png'
+date: '2023-10-13'
+ogImage:
+  url: '/assets/blog/001/white.png'
+tags:
+  - 'general'
+---
+
+# kazu-gorです。
+
+
+
--- src/config/links.tsx
+++ src/config/links.tsx
@@ -1,8 +1,9 @@
 import { AiOutlineInfoCircle } from 'react-icons/ai';
-import { FiTag } from 'react-icons/fi';
+import { FiTag, FiWind } from 'react-icons/fi';
 import { LinkType } from '@/types/link';
 
 export const links: LinkType[] = [
   { name: 'tags', href: '/tags', icon: <FiTag /> },
   { name: 'about', href: '/about', icon: <AiOutlineInfoCircle /> },
+  { name: 'sauna', href: '/sauna', icon: <FiWind /> },
 ];
